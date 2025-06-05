385ae7a69d4cc9bb22ad0e928bd3680cafeb2e47
Hank Yu <hank199599@gmail.com>
2025-06-05 13:11:01 +0800

    chore: update prompt content-5

--- functions/src/service/index.ts
+++ functions/src/service/index.ts
@@ -53,16 +53,17 @@ export class ContentGeneratorService implements ContentGeneratorServiceInterface
 
     async translateContent(title: string, description: string): Promise<TransformSocialMediaContent> {
         const systemPrompt = [
-            `請使用繁體中文，並以純文字格式（不包含任何註解或 Markdown）完成以下任務：
-
-1. 翻譯輸入的標題
-2. 翻譯輸入的摘要
-3. 提供五個與文章主題相關的標籤，格式為：#標籤 #標籤 #標籤 #標籤 #標籤
-4. 請直接輸出結果，不需任何額外說明或格式符號。
-`
+            `你是一位專業的語意分析與知識萃取模型，擅長從文章中理解深層脈絡、抽象主題、整合邏輯與提煉精要。你的任務是協助社群小編理解輸入的文章內容，並提供主題歸納與邏輯分析，作為後續撰寫摘要與社群貼文的基礎。請以全盤理解、深度解析與語意脈絡重建的角度，針對輸入文章進行分析，並提供如下資訊：
+1. 該文章的核心主題為何？是否包含多重議題？
+2. 文章重點為何？主要論點或情緒線索是如何鋪陳？
+3. 有無特定事件、案例、數據或觀點被強調？其背後意義為何？`
         ];
 
         const userPrompt = [
+            `請使用繁體中文，並以純文字格式完成下列任務：
+            1. 翻譯輸入的標題
+            2. 翻譯輸入的摘要
+            3. 請直接輸出結果，不需任何額外說明或格式符號。`,
             "輸入的標題為：" + title,
             "輸入的摘要為：" + description,
         ];
